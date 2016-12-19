930434cdcfc4ae65862d9bb679a7967548d9713e
Soreine <soreine.plume@gmail.com>
2016-12-19 16:01:45 +0100

    Add tests and ES6 support

---
+++ tests/history.js
@@ -0,0 +1,77 @@
+const expect = require('expect');
+const History = require('../');
+
+describe('History', () => {
+
+    // All the Histories in these test contain the serie of natural
+    // integer as successive states.
+    // 0, 1, 2, 3 etc.
+
+    it('should init', () => {
+        const history = History.create();
+        expect(history).toBeA(History);
+        expect(history.undos.isEmpty()).toBe(true);
+        expect(history.redos.isEmpty()).toBe(true);
+    });
+
+    it('should do', () => {
+        const history = History
+                  .create()
+                  .push(0);
+
+        expect(history.undos.count()).toEqual(1);
+        expect(history.redos.count()).toEqual(0);
+    });
+
+    it('should undo', () => {
+        const history = History
+                  .create()
+                  .push(0);
+
+
+        expect(history.previous).toEqual(0);
+
+        expect(history.undo(1).undos.count()).toEqual(0);
+        expect(history.undo(1).redos.count()).toEqual(1);
+    });
+
+    it('should redo', () => {
+        const history = History
+                  .create()
+                  .push(0)
+                  .undo(1);
+
+
+        expect(history.next).toEqual(1);
+
+        expect(history.redo(0).undos.count()).toEqual(1);
+        expect(history.redo(0).redos.count()).toEqual(0);
+    });
+
+    it('should drop redos on push', () => {
+        const history = History
+                  .create()
+                  .push(0)
+                  .undo(1)
+                  .push(0);
+
+        expect(history.previous).toEqual(0);
+
+        expect(history.undos.count()).toEqual(1);
+        expect(history.redos.count()).toEqual(0);
+    });
+
+    it('should handle max undo size', () => {
+        // Serie of natural integers
+        const serie = [...Array(1000).keys()];
+        const history = serie.reduce(
+            (h, n) => h.push(n),
+            History.create()
+        );
+
+        expect(history.previous).toEqual(999);
+
+        expect(history.undos.count()).toEqual(500);
+        expect(history.redos.count()).toEqual(0);
+    });
+});
